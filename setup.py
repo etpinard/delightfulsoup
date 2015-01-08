006d6b06b110451427e5f2ffb42004faf29a2679
etpinard <etienne.t.pinard@gmail.com>
2015-01-08 16:38:59 -0500

    fix typo again

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 exec (open('delightfulsoup/version.py').read())
 
-setup(name='djangofy',
+setup(name='delightfulsoup',
       version=__version__,
       author='Étienne Tétreault-Pinard',
       author_email='etienne.t.pinard@gmail.com',
       maintainer='Étienne Tétreault-Pinard',
       maintainer_email='etienne.t.pinard@gmail.com',
-      url='https://github.com/etpinard/djangofy',
+      url='https://github.com/etpinard/delightfulsoup',
       description="Python package extending BeautifulSoup's features",
       classifiers=[
           'Programming Language :: Python :: 2.7'
